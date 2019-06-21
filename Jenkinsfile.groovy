59078aab9a1ad9080b7f011cee4aee05617ec570
alexander <alexander@fancydsp.com>
2019-06-21 11:25:03 +0800

    upd

---
+++ Jenkinsfile.groovy
@@ -0,0 +1,14 @@
+node {
+    def mvnHome
+    stage('Preparation') {
+        git branch: 'master', credentialsId: 'git_account', url: 'http://xiebin@git.fancydsp.com/scm/data/jmx_exporter.git'
+        mvnHome = tool 'M3'
+    }
+    stage('Build') {
+        if (isUnix()) {
+            sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean install -DskipTests"
+        } else {
+            bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore -DskipTests clean package /)
+        }
+    }
+}
